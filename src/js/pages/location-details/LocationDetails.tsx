3a93c3efedf9ea848a24ffd5d842b05b408c0666
karihold <holdcroftkari@gmail.com>
2023-05-20 00:05:11 +0200

    removed console.log

--- src/js/pages/location-details/LocationDetails.tsx
+++ src/js/pages/location-details/LocationDetails.tsx
@@ -3,7 +3,7 @@ import { useParams } from 'react-router-dom';
 
 const LocationDetails = () => {
   const { locationName } = useParams<{ locationName: string }>();
-  console.log('Fyrer denne av?');
+
   return (
     <section>
       <h1>Welcomet to {locationName}</h1>
