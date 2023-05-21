6a3e3465c68b2bf41e6370aadf5d2e2287db9120
karihold <holdcroftkari@gmail.com>
2023-05-21 17:56:23 +0200

    renamed DegreeLabel to TempereatureLabel and coupled it to the personalization context

---
+++ src/js/ui/components/unit-labels/TemperatureLabel.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import { TemperatureUnit, usePersonalization } from '../../../contexts/personalization-context';
+import { convertTemperature } from '../../../utils/personalization-utils';
+
+type TemperatureLabelProps = {
+  value: number;
+  unit?: TemperatureUnit;
+};
+
+const TemperatureUnitSymbols: { [key in TemperatureUnit]: string } = {
+  celsius: 'C',
+  fahrenheit: 'F',
+  kelvin: 'K',
+};
+
+const TemperatureLabel = ({ value }: TemperatureLabelProps) => {
+  const { temperatureUnit } = usePersonalization();
+
+  // Temperature value and html degree symbol code (&#176;)
+  return (
+    <span>
+      {convertTemperature(value, temperatureUnit)}
+      <span>{TemperatureUnitSymbols[temperatureUnit]}&#176;</span>
+    </span>
+  );
+};
+
+export default TemperatureLabel;
