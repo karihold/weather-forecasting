03567a75b3f5ef3d2c8172021bc62c11f34cd6b9
karihold <holdcroftkari@gmail.com>
2023-05-21 12:10:20 +0200

    made InputForm component

---
+++ src/js/ui/components/input-form/InputForm.tsx
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+
+type InputFormProps = {
+  onSubmit: (value: string) => void | Promise<void>;
+  submitLabel: string;
+};
+
+const InputForm = ({ onSubmit, submitLabel = 'Submit' }: InputFormProps) => {
+  const [value, setValue] = useState<string>('');
+
+  return (
+    <form
+      onSubmit={(event) => {
+        event.preventDefault();
+        onSubmit(value);
+      }}
+    >
+      <input
+        value={value}
+        onChange={(event) => setValue(event.target.value)}
+      />
+      <button type="submit">{submitLabel}</button>
+    </form>
+  );
+};
+
+export default InputForm;
