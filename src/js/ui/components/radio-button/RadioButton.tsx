19313a170c2bf725d08273132be939559dff1851
karihold <holdcroftkari@gmail.com>
2023-05-22 13:24:26 +0200

    added id for radiobuttons

--- src/js/ui/components/radio-button/RadioButton.tsx
+++ src/js/ui/components/radio-button/RadioButton.tsx
@@ -3,6 +3,7 @@ import { ChangeEvent } from 'react';
 import './RadioButton.scss';
 
 export type RadioButtonProps<T extends string | number> = {
+  id?: string;
   label: string;
   name?: string;
   value: T;
@@ -11,15 +12,22 @@ export type RadioButtonProps<T extends string | number> = {
 };
 
 const RadioButton = <T extends string | number>({
+  id,
   label,
   name,
   value,
   onChange,
   isChecked,
 }: RadioButtonProps<T>) => {
+  const inputId = id ? id : `${name}-${value}`;
+
   return (
-    <label className="radio-label">
+    <label
+      className="radio-label"
+      htmlFor={inputId}
+    >
       <input
+        id={inputId}
         className="radio-button"
         name={name}
         type="radio"
